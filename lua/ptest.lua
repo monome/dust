587170d3a820aceacbe9bdebc66178c4b842f4b8
brian crabtree <tehn@monome.org>
2018-04-10 15:46:14 -0400

    ptest following paramset updates

--- lua/ptest.lua
+++ lua/ptest.lua
@@ -1,40 +1,44 @@
 engine = 'TestSine'
 
-spec = controlspec.new(0, 100, 'lin', 1, 0, "bits", false, 1, 1)
-
 init = function()
-  p.number = param.new("number")
-  p.something = param.new("something",spec)
-  p.freq = param.new("freq",controlspec.freq())
-  p.freq.action = e.hz
-  p.bi = param.new("bi",controlspec.bipolar())
-  local l = {"true", "false"}
-  p.news = option.new("news",l)
-  p.news.action = function(i) print(p.news:string()) end
-  p.mode = option.new("mode",{"MIDI","OSC","SYNTH","CV"})
-  paramset.read(p,"ptest.pset")
-  paramset.bang(p)
+  p:add_number("num")
+  p:add_option("output",{"MIDI","OSC","SYNTH","CV"})
+  p:add_param("something",controlspec.unipolar())
+  p:add_param("freq",controlspec.freq())
+  p:action("freq",e.hz)
+  
+  p:read("ptest.pset")
 end
 
 key = function(n,z)
   if z==1 then
     if n == 2 then
-      print("-- "..p.freq:get())
+      p:read("ptest.pset")
     elseif n==3 then
-      p.news:delta(1)
+      p:write("ptest.pset")
     end
   end
+  redraw()
 end
 
 enc = function(n,d)
   if n == 2 then
-    p.freq:delta(d/10)
-  elseif n==1 then
-    p.news:delta(d)
+    p:delta("freq",d/10)
+  elseif n==3 then
+    p:delta("freq",d)
   end
+  redraw()
+end
+
+redraw = function()
+  s.clear()
+  s.move(0,10)
+  s.level(15)
+  s.text("freq: "..p:string("freq"))
+  s.update()
 end
 
 
 cleanup = function() 
-  paramset.write(p,"ptest.pset")
+  p:write("ptest.pset")
 end
