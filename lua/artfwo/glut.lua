1c8c0b00e57dd9b313934c5967492877a43ce6e8
Artem Popov <artfwo@gmail.com>
2018-04-29 22:07:29 +0700

    improve phase interpolation

--- lua/artfwo/glut.lua
+++ lua/artfwo/glut.lua
@@ -1,6 +1,6 @@
 -- glut
 --
--- granular sampler
+-- granular sampler in progress
 --
 -- ////////
 -- ////
@@ -17,8 +17,8 @@ local VOICES = 7
 
 local positions = {-1, -1, -1, -1, -1, -1, -1}
 local gates = {0, 0, 0, 0, 0, 0, 0}
-local focus = 1
-local param_focus = 1
+local current_voice = 1
+local current_param = 1
 local param_names = {
   "rate",
   "jitter",
@@ -33,34 +33,31 @@ local grid_ctl = gridbuf.new(16, 8)
 local grid_voc = gridbuf.new(16, 8)
 
 local function ledinterp(value, width)
-  local pos = value * (width - 1) + 0.5
+  local pos = value * width
 
   local levels = {}
   for i = 1, width do
     levels[i] = 0
   end
 
-  local left = math.floor(pos - 0.5)
+  local left = math.floor(pos)
   local index_left = left + 1
-  local dist_left = math.abs(pos - (left + 0.5))
+  local dist_left = math.abs(pos - left)
 
-  local right = math.floor(pos + 0.5)
+  local right = math.floor(pos + 1)
   local index_right = right + 1
-  local dist_right = math.abs(pos - (right + 0.5))
+  local dist_right = math.abs(pos - right)
 
-  if index_right > width then
-    index_right = 1
-  end
+  if index_left < 1 then index_left = width end
+  if index_left > width then index_left = 1 end
 
-  levels[index_left] = math.floor((1 - dist_left) * 15)
-  levels[index_right] = math.floor((1 - dist_right) * 15)
+  if index_right < 1 then index_right = width end
+  if index_right > width then index_right = 1 end
+
+  levels[index_left] = math.floor(math.abs(1 - dist_left) * 15)
+  levels[index_right] = math.floor(math.abs(1 - dist_right) * 15)
 
   return levels
-end
-
-local function update_pos(voice, pos)
-  --local led_pos = math.floor(pos * 16) + 1
-  positions[voice] = pos
 end
 
 local function start_voice(voice, pos)
@@ -76,7 +73,7 @@ end
 
 local function fileselect_callback(path)
   if path ~= "cancel" then
-    engine.read(focus, path)
+    engine.read(current_voice, path)
   end
 end
 
@@ -89,7 +86,7 @@ local function gridredraw()
   grid_voc:led_level_all(0)
 
   for i=1, 16 do
-    grid_ctl:led_level_set(i, focus + 1, 3)
+    grid_ctl:led_level_set(i, current_voice + 1, 3)
   end
 
   for i=1, 7 do
@@ -104,15 +101,7 @@ local function gridredraw()
   g:refresh()
 end
 
--- init function
 function init()
-  engine.list_commands()
-  -- set engine params
-  engine.read(1, "/usr/share/sounds/alsa/Front_Left.wav")
-  engine.read(2, "/usr/share/sounds/alsa/Front_Right.wav")
-  engine.read(3, "/usr/share/sounds/alsa/Rear_Left.wav")
-  engine.read(4, "/usr/share/sounds/alsa/Rear_Right.wav")
-
   -- grid refresh timer
   c = metro[1]
   c.count = -1
@@ -127,10 +116,16 @@ function init()
     params:add_file("sample"..v)
     params:set_action("sample"..v, function(file) engine.read(v, file) end)
 
-    p = poll.set('phase_' .. v, function(pos) update_pos(v, pos) end)
+    p = poll.set('phase_' .. v, function(pos) positions[v] = pos end)
     p.time = 0.05
     p:start()
   end
+
+  -- load default sounds
+  engine.read(1, "/usr/share/sounds/alsa/Front_Left.wav")
+  engine.read(2, "/usr/share/sounds/alsa/Front_Right.wav")
+  engine.read(3, "/usr/share/sounds/alsa/Rear_Left.wav")
+  engine.read(4, "/usr/share/sounds/alsa/Rear_Right.wav")
 
   for v = 1, VOICES do
 
@@ -152,15 +147,16 @@ function init()
     params:add_control("spread"..v, controlspec.new(0, 1, "lin", 0, 0, ""))
     params:set_action("spread"..v, function(value) engine.spread(v, value) end)
   end
+
+  params:bang()
 end
 
--- grid key function
 function gridkey(x, y, state)
   if state > 0 then
     -- set voice pos
     if y > 1 then
       local voice = y - 1
-      start_voice(voice, (x - 1) / 16.0)
+      start_voice(voice, (x - 1) / 16)
     else
       local voice = x
       stop_voice(voice)
@@ -170,13 +166,13 @@ end
 
 function enc(n, d)
   if n == 1 then
-    focus = focus + d
-    if focus > 7 then focus = 7 end
-    if focus < 1 then focus = 1 end
+    current_voice = current_voice + d
+    if current_voice > 7 then current_voice = 7 end
+    if current_voice < 1 then current_voice = 1 end
   elseif n == 2 then
-    param_focus = util.clamp(param_focus + d, 1, 6)
+    current_param = util.clamp(current_param + d, 1, 6)
   elseif n == 3 then
-    params:delta(param_names[param_focus]..focus, d / 10)
+    params:delta(param_names[current_param]..current_voice, d / 10)
   end
   redraw()
 end
@@ -192,31 +188,31 @@ function redraw()
 
   screen.level(5)
   screen.move(127, 10)
-  screen.text_right("voice: "..focus)
+  screen.text_right("voice: "..current_voice)
 
-  if param_focus == 1 then screen.level(15) else screen.level(5) end
+  if current_param == 1 then screen.level(15) else screen.level(5) end
   screen.move(0, 10)
-  screen.text("speed: "..params:string("rate"..focus))
+  screen.text("speed: "..params:string("rate"..current_voice))
 
-  if param_focus == 2 then screen.level(15) else screen.level(5) end
+  if current_param == 2 then screen.level(15) else screen.level(5) end
   screen.move(0, 20)
-  screen.text("jitter: "..params:string("jitter"..focus))
+  screen.text("jitter: "..params:string("jitter"..current_voice))
 
-  if param_focus == 3 then screen.level(15) else screen.level(5) end
+  if current_param == 3 then screen.level(15) else screen.level(5) end
   screen.move(0, 30)
-  screen.text("dur: "..params:string("dur"..focus))
+  screen.text("dur: "..params:string("dur"..current_voice))
 
-  if param_focus == 4 then screen.level(15) else screen.level(5) end
+  if current_param == 4 then screen.level(15) else screen.level(5) end
   screen.move(0, 40)
-  screen.text("density: "..params:string("density"..focus))
+  screen.text("density: "..params:string("density"..current_voice))
 
-  if param_focus == 5 then screen.level(15) else screen.level(5) end
+  if current_param == 5 then screen.level(15) else screen.level(5) end
   screen.move(0, 50)
-  screen.text("pitch: "..params:string("pitch"..focus))
+  screen.text("pitch: "..params:string("pitch"..current_voice))
 
-  if param_focus == 6 then screen.level(15) else screen.level(5) end
+  if current_param == 6 then screen.level(15) else screen.level(5) end
   screen.move(0, 60)
-  screen.text("spread: "..params:string("spread"..focus))
+  screen.text("spread: "..params:string("spread"..current_voice))
 
   screen.level(5)
   screen.move(127, 60)
