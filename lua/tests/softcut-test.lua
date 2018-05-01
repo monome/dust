840d56f7eb91e346b8270f08dc117bfb30c14d18
emb <emb@catfact.net>
2018-04-30 22:25:50 -0400

    softcut-test: use buffer duration poll, add some uI

--- lua/tests/softcut-test.lua
+++ lua/tests/softcut-test.lua
@@ -11,6 +11,12 @@ audio.monitor_off()
 
 engine.name = 'SoftCut'
 
+-- state variables
+
+local rate = 1
+local rec_state = 1
+local pre_rec = 0.75
+
 init = function()
   engine.list_commands()
 
@@ -38,56 +44,87 @@ init = function()
   engine.rec_on(1, 1)
   engine.start(1)
 
-  engine.rec_lag(1, 0.125)
-  engine.pre_lag(1, 0.125)
-
+  engine.rec_lag(1, 1/16)
+  engine.pre_lag(1, 1/16)
+  engine.rate_lag(1, 1/16)
+  
+  engine.fade(1, 0.5)
+  
   -- poll the quantized phase (only fires when quantized phase changes)
-  local p = poll.set('phase_quant_1', update_phase)
-  p:start()
+  local p_phase = poll.set('phase_quant_1', update_phase)
+  p_phase:start()
+
+  -- poll the buffer duration
+  local p_buf_dur = poll.set('buf_dur', function(dur) print("buffer duration: " .. dur) end)
+  -- request an update immediately
+  p_buf_dur:update() 
+  
+  if g ~= nil then g:all(0) end
+  
+  -- why doesn't this work here?
+  screen_redraw()
+
 end
 
-local rate = 1
 enc = function(n, d)
    if n == 2 then
-      rate = rate + (d * 0.0625)
+      rate = rate + (d / 16)
       if rate > 4 then rate = 4 end
       if rate < -4 then rate = -4 end
       engine.rate(1, rate)
+      screen_redraw()
+   end
+   if n == 3 then
+      pre_rec = pre_rec + (d / 16)
+      if pre_rec < 0 then pre_rec = 0 end
+      if pre_rec > 1 then pre_rec = 1 end
+      engine.pre(1, pre_rec)
+      screen_redraw()
    end
 end
 
-local rec_state = 1
-local pre_rec = 0.75
 key = function(n, z)
    if n == 2 then
       if z > 0 then	 
 	 rec_state = 1 - rec_state
-	 if rec_state > 0 then 	    
+	 if rec_state > 0 then
 	    engine.rec(1, 1)
 	    engine.pre(1, pre_rec)
 	 else
 	    engine.rec(1, 0)
 	    engine.pre(1, 1)
 	 end
-	 redraw_screen()
+	 screen_redraw()
       end
    end
    if n == 3 then
       if z > 0 then
 	 rate = 1
 	 engine.rate(1, rate)
+	 screen_redraw()
       end
    end
 end
 
-screen:level(4)
-redraw_screen = function()
+screen_redraw = function()
    screen.clear()
-   screen.move(0, 0)
+   screen.level(10)
+   screen.move(40, 20)
+   
    if rec_state > 0 then
-      screen.rect(0, 0, 128, 64)
+      screen.text("rec: on")
+      screen.move(1, 1)
+      screen.rect(1, 1, 10, 10)
       screen.fill()
+   else 
+      screen.text("rec: off")
    end
+   
+   screen.move(40, 30)
+   screen.text("pre: " .. pre_rec)
+   screen.move(40, 40)
+   screen.text("rate: " .. rate)
+
    screen.update()
 end
 
@@ -95,11 +132,17 @@ end
 local loop_start = 1
 local gpos = 1
 update_phase = function(phase)
+   -- print(phase)
    if g ~= nil then
       g:led(gpos, 1, 0)
-      gpos = (phase / 48000 - loop_start) * 7.99       
+      gpos = (phase - loop_start) * 8 + 1
+      if(gpos > 16) then gpos = 16 end
       g:led(gpos, 1, 12)
       g:refresh()
    end
    
 end
+
+
+-- why doesn't this work here either??
+screen_redraw()
