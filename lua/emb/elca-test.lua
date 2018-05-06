39f2368386a44b94101ee520df532b06f2268394
emb <emb@catfact.net>
2018-05-06 18:53:44 -0400

    elca sines (requires  norns branch)

--- lua/emb/elca-test.lua
+++ lua/emb/elca-test.lua
@@ -7,20 +7,22 @@
 -- key 3 : start
 -- enc 2 : rate
 --
---- TODO: sounds...?
 
 local elca = require 'emb.elca'
 
 local ca = elca.new() -- CA state
-local seq = metro[1] -- main timer
+local seq = metro.alloc() -- main timer
 
 local n = 8 -- length of window
 local m = 16 -- length of history
 
+
+engine.name = 'Sines'
+
 ca.state[1] = 1
 ca.state[5] = 1
 ca.state[8] = 1
-ca.rule = 110
+ca.rule = 126
 
 ca.bound_mode_l = elca.BOUND_WRAP
 ca.bound_mode_r = elca.BOUND_LOW
@@ -29,25 +31,33 @@ ca.bound_r = elca.NUM_STATES
 
 local history = {}
 for i=1,m do
-   print ('i='..i)
    local col = {}
    for j=1,n do
-      print('j='..j)
       col[j] = 0
    end
    table.insert(history, col)
 end
 
-function gridredraw()
+function refresh()
    if g == nil then return end
    local val
    for i=1, 16 do
       if i == 16 then val = 12 else val = 4 end
       local col = history[i]
-      local z
+      local z, amp
       for j=1,8 do
-         if col[j] > 0 then z = val else z = 0 end
-	 g:led(i, j, col[j])
+         if col[j] > 0 then
+	    z = val
+	    amp =  0.0625
+	 else
+	    z = 0
+	    amp = 0
+	 end
+	 g:led(i, j, z)
+	 if i == 16 then
+	    -- fixme very bad structre to to this here
+	    engine.amp(j, amp)
+	 end
       end
    end
    g:refresh()
@@ -59,8 +69,7 @@ gridkey = function(x, y, z)
    -- most recent row - set the state
    if x == 16 then
       if ca.state[y] > 0 then ca.state[y] = 0 else ca.state[y] = 1 end
-   else
-      
+   else      
       -- earlier rows - change the rule such that it would have produced a different value
       -- (and change the state too)
       local col = history[x-1]
@@ -75,7 +84,7 @@ gridkey = function(x, y, z)
       ca:set_rule_by_state(history[x][y], l, c, r)
       print("rule by state: ", history[x][y], l, c, r)
       print("new rule: ", ca.rule)
-      gridredraw()
+      refresh()
    end
 end
 
@@ -88,29 +97,42 @@ seq.callback = function(stage)
    table.insert(history, col)
    table.remove(history, 1)
 
-   gridredraw()
+   refresh()
 
+   -- check GC
+   local kb = math.floor(collectgarbage("count"))
+   local str = "used: " .. kb .. " kB"
+   
+   -- current menu/script redrawing means that actually doing this will break the menu
    --[[
-   local str = ""
-   for i=1,8 do      
-      if col[i] > 0 then str = str .. "0" else str = str .. "." end
-   end
-   print(str)
+   screen.clear()
+   screen.move(10, 10)
+   screen.text(str)
+   screen.update()
    --]]
+
    
 end
 
-engine.name = 'TestSine'
+--engine.name = 'TestSine'
+
 init = function()
    
-   print("grid: ", g)
+   -- wrapped harmonix
+   for i=1,8 do
+      local hz = 55 * (i+3)
+      while hz > 1000 do hz = hz / 2 end
+      print(hz)
+      engine.hz(i, hz)
+      -- without this sleep, some of the messages seem to get lost
+      usleep(10000)
+   end
    
    seq.time = 0.125
    seq:start()
 end
 
-function copy_history(newcol) 
-	 
+function copy_history(newcol)
    for x in 1,n do
    end
 end
