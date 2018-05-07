9b9850538b9619ea098c42715c613803d24ef3ea
brian crabtree <tehn@monome.org>
2018-05-07 14:12:33 -0400

    grid_seek fix

--- data/system.lua
+++ data/system.lua
@@ -1,6 +1,6 @@
 -- system state
 norns.state.script = 'poly.lua'
-norns.state.out = '54'
+norns.state.out = '46.0'
 norns.state.monitor = '0'
 norns.state.monitor_mode = '0'
 norns.state.input_left = '48.0'
