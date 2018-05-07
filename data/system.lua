ad529838a6aeb4a92e8e894c771739efc7f7529e
brian crabtree <tehn@monome.org>
2018-05-06 21:12:30 -0400

    poly ui fixes

--- data/system.lua
+++ data/system.lua
@@ -1,8 +1,8 @@
 -- system state
 norns.state.script = 'poly.lua'
-norns.state.out = '31.0'
+norns.state.out = '54'
 norns.state.monitor = '0'
 norns.state.monitor_mode = '0'
 norns.state.input_left = '48.0'
 norns.state.input_right = '48.0'
-norns.state.hp = '40'
+norns.state.hp = '50.0'
