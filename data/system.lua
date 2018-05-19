c525523ec0030dd28262e16e106cac8fdb445ef1
ezra buchla <emb@catfact.net>
2018-05-19 15:16:50 -0400

    remove settings from the repo

--- data/system.lua
+++
@@ -1,8 +0,0 @@
--- system state
-norns.state.script = 'emb/gold-chains.lua'
-norns.state.out = '63'
-norns.state.monitor = '0'
-norns.state.monitor_mode = '0'
-norns.state.input_left = '40.0'
-norns.state.input_right = '40.0'
-norns.state.hp = '43'
