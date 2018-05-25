3e9c11c0a8e75480e075e413f1e7e0232d42b666
Anton Hörnquist <anton.hornquist@gmail.com>
2018-05-25 10:26:09 +0200

    use metro.alloc()

--- scripts/jah/step.lua
+++ scripts/jah/step.lua
@@ -180,7 +180,7 @@ function init()
     end
   end
 
-  timer = metro[1] -- TODO: this is probably no longer the way to spawn metros(?)
+  timer = metro.alloc()
   timer.callback = tick
 
   params:add_option("grid width", {"8", "16"}, 2) -- TODO: should now be possible to infer from grid metadata(?)
