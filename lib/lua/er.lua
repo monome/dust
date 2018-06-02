96633736865346cc29c8b31b393f34a86ea9d3f5
emb <emb@catfact.net>
2018-06-02 19:01:21 -0400

    simplify

--- lib/lua/er.lua
+++ lib/lua/er.lua
@@ -4,9 +4,6 @@
 -- @param k : number of pulses
 -- @param n : total number of steps
 function er(k, n)
-
-   -- total number of steps
-   local m = k - n
    -- results array, intialliy all zero
    local r = {}
    for i=1,n do r[i] = false end
@@ -14,18 +11,13 @@ function er(k, n)
    -- using the "bucket method"
    -- for each step in the output, add K to the bucket.
    -- if the bucket overflows, this step contains a pulse.
-   local b = 0
+   local b = n
    for i=1,n do
-      b = b + k
       if b >= n then
 	 b = b - n
-	 -- r[i] = true
-	 --- hm, let's rotate left by 1 (or forward by N)
-	 --- this means that pulse will always be on first step, instead of last step
-	 r[(i+n+1)%n] = true
-      end
-   end
-
-   return r
-   
+	 r[i] = true
+      end      
+      b = b + k
+   end   
+   return r   
 end
