4755245020353429404a9cfd4776e4f1e2f30519
Anton Hörnquist <anton.hornquist@gmail.com>
2018-06-06 23:03:05 +0200

    more gong

--- lib/lua/jah/gong.lua
+++ lib/lua/jah/gong.lua
@@ -1,44 +1,44 @@
 local ControlSpec = require 'controlspec'
 local Formatters = require 'jah/formatters'
-local Ack = {}
+local Gong = {}
 
-Ack.specs = {}
+Gong.specs = {}
 
-Ack.specs.send = ControlSpec.DB:copy()
-Ack.specs.send.default = -60
+Gong.specs.send = ControlSpec.DB:copy()
+Gong.specs.send.default = -60
 
-Ack.specs.sample_start = ControlSpec.UNIPOLAR
-Ack.specs.sample_end = ControlSpec.new(0, 1, 'lin', 0, 1, "")
-Ack.specs.loop_point = ControlSpec.UNIPOLAR
+Gong.specs.sample_start = ControlSpec.UNIPOLAR
+Gong.specs.sample_end = ControlSpec.new(0, 1, 'lin', 0, 1, "")
+Gong.specs.loop_point = ControlSpec.UNIPOLAR
 
-Ack.specs.speed = ControlSpec.new(0.05, 5, 'lin', 0, 1, "")
--- Ack.specs.slew = ControlSpec.new(0, 5, 'lin', 0, 0, "") -- TODO: slews
+Gong.specs.speed = ControlSpec.new(0.05, 5, 'lin', 0, 1, "")
+-- Gong.specs.slew = ControlSpec.new(0, 5, 'lin', 0, 0, "") -- TODO: slews
 
-Ack.specs.volume = ControlSpec.DB:copy()
-Ack.specs.volume.default = -10
+Gong.specs.volume = ControlSpec.DB:copy()
+Gong.specs.volume.default = -10
 
-Ack.specs.volume_env_attack = ControlSpec.new(0, 1, 'lin', 0, 0.001, "secs")
-Ack.specs.volume_env_release = ControlSpec.new(0, 3, 'lin', 0, 3, "secs")
-Ack.specs.filter_env_attack = ControlSpec.new(0, 1, 'lin', 0, 0.001, "secs")
-Ack.specs_filter_env_release = ControlSpec.new(0, 3, 'lin', 0, 0.25, "secs")
+Gong.specs.volume_env_attack = ControlSpec.new(0, 1, 'lin', 0, 0.001, "secs")
+Gong.specs.volume_env_release = ControlSpec.new(0, 3, 'lin', 0, 3, "secs")
+Gong.specs.filter_env_attack = ControlSpec.new(0, 1, 'lin', 0, 0.001, "secs")
+Gong.specs_filter_env_release = ControlSpec.new(0, 3, 'lin', 0, 0.25, "secs")
 
-Ack.specs.filter_cutoff = ControlSpec.FREQ:copy()
-Ack.specs.filter_cutoff.default = 20000
+Gong.specs.filter_cutoff = ControlSpec.FREQ:copy()
+Gong.specs.filter_cutoff.default = 20000
 
-Ack.specs.filter_res = ControlSpec.UNIPOLAR
-Ack.specs.filter_env_mod = ControlSpec.BIPOLAR
+Gong.specs.filter_res = ControlSpec.UNIPOLAR
+Gong.specs.filter_env_mod = ControlSpec.BIPOLAR
 
-Ack.specs.delay_time = ControlSpec.new(0.0001, 5, 'exp', 0, 0.1, "secs")
-Ack.specs.delay_feedback = ControlSpec.new(0, 1.25, 'lin', 0, 0.5, "")
-Ack.specs.delay_level = ControlSpec.DB:copy()
-Ack.specs.delay_level.default = -10
+Gong.specs.delay_time = ControlSpec.new(0.0001, 5, 'exp', 0, 0.1, "secs")
+Gong.specs.delay_feedback = ControlSpec.new(0, 1.25, 'lin', 0, 0.5, "")
+Gong.specs.delay_level = ControlSpec.DB:copy()
+Gong.specs.delay_level.default = -10
 
-Ack.specs.reverb_room = ControlSpec.new(0, 1, 'lin', 0, 0.5, "")
-Ack.specs.reverb_damp = ControlSpec.new(0, 1, 'lin', 0, 0.5, "")
-Ack.specs.reverb_level = ControlSpec.DB:copy()
-Ack.specs.reverb_level.default = -10
+Gong.specs.reverb_room = ControlSpec.new(0, 1, 'lin', 0, 0.5, "")
+Gong.specs.reverb_damp = ControlSpec.new(0, 1, 'lin', 0, 0.5, "")
+Gong.specs.reverb_level = ControlSpec.DB:copy()
+Gong.specs.reverb_level.default = -10
 
-function Ack.add_channel_sample_param(channel)
+function Gong.add_channel_sample_param(channel)
   params:add_file(channel..": sample")
   params:set_action(channel..": sample", function(value)
     if value ~= "-" then
@@ -47,17 +47,17 @@ function Ack.add_channel_sample_param(channel)
   end)
 end
 
-function Ack.add_start_pos_param(channel)
-  params:add_control(channel..": start pos", Ack.specs.sample_start, Formatters.unipolar_as_percentage)
+function Gong.add_start_pos_param(channel)
+  params:add_control(channel..": start pos", Gong.specs.sample_start, Formatters.unipolar_as_percentage)
   params:set_action(channel..": start pos", function(value) engine.sampleStart(channel-1, value) end)
 end
 
-function Ack.add_end_pos_param(channel)
-  params:add_control(channel..": end pos", Ack.specs.sample_end, Formatters.unipolar_as_percentage)
+function Gong.add_end_pos_param(channel)
+  params:add_control(channel..": end pos", GonGong.specs.sample_end, Formatters.unipolar_as_percentage)
   params:set_action(channel..": end pos", function(value) engine.sampleEnd(channel-1, value) end)
 end
 
-function Ack.add_loop_param(channel)
+function Gong.add_loop_param(channel)
   params:add_option(channel..": loop", {"off", "on"})
   params:set_action(channel..": loop", function(value)
     if value == 2 then
@@ -68,95 +68,95 @@ function Ack.add_loop_param(channel)
   end)
 end
 
-function Ack.add_loop_point_param(channel)
-  params:add_control(channel..": loop point", Ack.specs.loop_point, Formatters.unipolar_as_percentage)
+function Gong.add_loop_point_param(channel)
+  params:add_control(channel..": loop point", Gong.specs.loop_point, Formatters.unipolar_as_percentage)
   params:set_action(channel..": loop point", function(value) engine.loopPoint(channel-1, value) end)
 end
 
-function Ack.add_speed_param(channel)
-  params:add_control(channel..": speed", Ack.specs.speed, Formatters.unipolar_as_percentage)
+function Gong.add_speed_param(channel)
+  params:add_control(channel..": speed", Gong.specs.speed, Formatters.unipolar_as_percentage)
   params:set_action(channel..": speed", function(value) engine.speed(channel-1, value) end)
 end
 
-function Ack.add_vol_param(channel)
-  params:add_control(channel..": vol", Ack.specs.volume, Formatters.default)
+function Gong.add_vol_param(channel)
+  params:add_control(channel..": vol", GonGong.specs.volume, Formatters.default)
   params:set_action(channel..": vol", function(value) engine.volume(channel-1, value) end)
 end
 
-function Ack.add_vol_env_atk_param(channel)
-  params:add_control(channel..": vol env atk", Ack.specs.volume_env_attack, Formatters.secs_as_ms)
+function Gong.add_vol_env_atk_param(channel)
+  params:add_control(channel..": vol env atk", Gong.specs.volume_env_attack, Formatters.secs_as_ms)
   params:set_action(channel..": vol env atk", function(value) engine.volumeEnvAttack(channel-1, value) end)
 end
 
-function Ack.add_vol_env_rel_param(channel)
-  params:add_control(channel..": vol env rel", Ack.specs.volume_env_release, Formatters.secs_as_ms)
+function Gong.add_vol_env_rel_param(channel)
+  params:add_control(channel..": vol env rel", Gong.specs.volume_env_release, Formatters.secs_as_ms)
   params:set_action(channel..": vol env rel", function(value) engine.volumeEnvRelease(channel-1, value) end)
 end
 
-function Ack.add_pan_param(channel)
+function Gong.add_pan_param(channel)
   params:add_control(channel..": pan", ControlSpec.PAN, Formatters.bipolar_as_pan_widget)
   params:set_action(channel..": pan", function(value) engine.pan(channel-1, value) end)
 end
 
-function Ack.add_filter_cutoff_param(channel)
-  params:add_control(channel..": filter cutoff", Ack.specs.filter_cutoff, Formatters.round(0.001))
+function Gong.add_filter_cutoff_param(channel)
+  params:add_control(channel..": filter cutoff", Gong.specs.filter_cutoff, Formatters.round(0.001))
   params:set_action(channel..": filter cutoff", function(value) engine.filterCutoff(channel-1, value) end)
 end
 
-function Ack.add_filter_res_param(channel)
-  params:add_control(channel..": filter res", Ack.specs.filter_res, Formatters.unipolar_as_percentage)
+function Gong.add_filter_res_param(channel)
+  params:add_control(channel..": filter res", Gong.specs.filter_res, Formatters.unipolar_as_percentage)
   params:set_action(channel..": filter res", function(value) engine.filterRes(channel-1, value) end)
 end
 
-function Ack.add_filter_mode_param(channel)
+function Gong.add_filter_mode_param(channel)
   params:add_option(channel..": filter mode", {"lowpass", "bandpass", "highpass", "notch", "peak"})
   params:set_action(channel..": filter mode", function(value) engine.filterMode(channel-1, value-1) end)
 end
 
-function Ack.add_filter_env_atk_param(channel)
-  params:add_control(channel..": filter env atk", Ack.specs.filter_env_attack, Formatters.secs_as_ms)
+function Gong.add_filter_env_atk_param(channel)
+  params:add_control(channel..": filter env atk", Gong.specs.filter_env_attack, Formatters.secs_as_ms)
   params:set_action(channel..": filter env atk", function(value) engine.filterEnvAttack(channel-1, value) end)
 end
 
-function Ack.add_filter_env_rel_param(channel)
-  params:add_control(channel..": filter env rel", Ack.specs_filter_env_release, Formatters.secs_as_ms)
+function Gong.add_filter_env_rel_param(channel)
+  params:add_control(channel..": filter env rel", Gong.specs_filter_env_release, Formatters.secs_as_ms)
   params:set_action(channel..": filter env rel", function(value) engine.filterEnvRelease(channel-1, value) end)
 end
 
-function Ack.add_filter_env_mod_param(channel)
-  params:add_control(channel..": filter env mod", Ack.specs.filter_env_mod, Formatters.bipolar_as_percentage)
+function Gong.add_filter_env_mod_param(channel)
+  params:add_control(channel..": filter env mod", Gong.specs.filter_env_mod, Formatters.bipolar_as_percentage)
   params:set_action(channel..": filter env mod", function(value) engine.filterEnvMod(channel-1, value) end)
 end
 
-function Ack.add_delay_send_param(channel)
-  params:add_control(channel..": delay send", Ack.specs.send, Formatters.default)
+function Gong.add_delay_send_param(channel)
+  params:add_control(channel..": delay send", Gong.specs.send, Formatters.default)
   params:set_action(channel..": delay send", function(value) engine.delaySend(channel-1, value) end)
 end
 
-function Ack.add_reverb_send_param(channel)
-  params:add_control(channel..": reverb send", Ack.specs.send, Formatters.default)
+function Gong.add_reverb_send_param(channel)
+  params:add_control(channel..": reverb send", GonGong.specs.send, Formatters.default)
   params:set_action(channel..": reverb send", function(value) engine.reverbSend(channel-1, value) end)
 end
 
-function Ack.add_channel_params(channel)
-  Ack.add_channel_sample_param(channel)
-  Ack.add_start_pos_param(channel)
-  Ack.add_end_pos_param(channel)
-  Ack.add_loop_param(channel)
-  Ack.add_loop_point_param(channel)
-  Ack.add_speed_param(channel)
-  Ack.add_vol_param(channel)
-  Ack.add_vol_env_atk_param(channel)
-  Ack.add_vol_env_rel_param(channel)
-  Ack.add_pan_param(channel)
-  Ack.add_filter_mode_param(channel)
-  Ack.add_filter_cutoff_param(channel)
-  Ack.add_filter_res_param(channel)
-  Ack.add_filter_env_atk_param(channel)
-  Ack.add_filter_env_rel_param(channel)
-  Ack.add_filter_env_mod_param(channel)
-  Ack.add_delay_send_param(channel)
-  Ack.add_reverb_send_param(channel)
+function Gong.add_channel_params(channel)
+  Gong.add_channel_sample_param(channel)
+  Gong.add_start_pos_param(channel)
+  Gong.add_end_pos_param(channel)
+  Gong.add_loop_param(channel)
+  Gong.add_loop_point_param(channel)
+  Gong.add_speed_param(channel)
+  Gong.add_vol_param(channel)
+  Gong.add_vol_env_atk_param(channel)
+  Gong.add_vol_env_rel_param(channel)
+  Gong.add_pan_param(channel)
+  Gong.add_filter_mode_param(channel)
+  Gong.add_filter_cutoff_param(channel)
+  Gong.add_filter_res_param(channel)
+  Gong.add_filter_env_atk_param(channel)
+  Gong.add_filter_env_rel_param(channel)
+  Gong.add_filter_env_mod_param(channel)
+  Gong.add_delay_send_param(channel)
+  Gong.add_reverb_send_param(channel)
 
   -- TODO: refactor each param into a separate function
   --[[
@@ -174,28 +174,113 @@ function Ack.add_channel_params(channel)
   ]]
 end
 
-function Ack.add_effects_params()
-  params:add_control("delay time", Ack.specs.delay_time, Formatters.secs_as_ms)
-  params:set_action("delay time", engine.delayTime)
-  params:add_control("delay feedback", Ack.specs.delay_feedback, Formatters.unipolar_as_percentage)
-  params:set_action("delay feedback", engine.delayFeedback)
-  params:add_control("delay level", Ack.specs.delay_level, Formatters.default)
-  params:set_action("delay level", engine.delayLevel)
-  params:add_control("reverb room size", Ack.specs.reverb_room, Formatters.unipolar_as_percentage)
-  params:set_action("reverb room size", engine.reverbRoom)
-  params:add_control("reverb damp", Ack.specs.reverb_damp, Formatters.unipolar_as_percentage)
-  params:set_action("reverb damp", engine.reverbDamp)
-  params:add_control("reverb level", Ack.specs.reverb_level, Formatters.default)
-  params:set_action("reverb level", engine.reverbLevel)
-end
+function Gong.add_params()
+  local numoscs = 3
+  local partial_spec = ControlSpec.new(0.5, 10, 'lin', 0.25, 1)
+  local index_spec = ControlSpec.new(0, 24, 'lin', 0, 3, "")
 
-function Ack.add_params()
-  for channel=1,8 do
-    Ack.add_channel_params(channel)
-    params:add_separator()
+  for oscnum=1,numoscs do
+    params:add_control("osc"..oscnum.." gain", ControlSpec.AMP)
+    params:set_action("osc"..oscnum.." gain", function(value) all_fm("osc"..oscnum.."gain", value) end)
+
+    params:add_option("osc"..oscnum.." type", {"partial", "fixed"})
+    params:set_action("osc"..oscnum.." type", osc_type_action)
+    params:add_control("osc"..oscnum.." partial no", partial_spec)
+    params:set_action("osc"..oscnum.." partial no", osc_type_action)
+    params:add_control("osc"..oscnum.." fixed freq", ControlSpec.WIDEFREQ)
+    params:set_action("osc"..oscnum.." fixed freq", osc_type_action)
+
+    params:add_control("osc"..oscnum.." index", index_spec)
+    params:set_action("osc"..oscnum.." index", function(value) all_fm("osc"..oscnum.."index", value) end)
+
+    params:add_control("osc"..oscnum.." > out", ControlSpec.UNIPOLAR)
+    params:set_action("osc"..oscnum.." > out", function(value) all_fm("osc"..oscnum.."outlevel", value) end)
+
+    params:add_control("osc"..src.." > osc"..oscnum.." freq", ControlSpec.UNIPOLAR)
+    params:set_action("osc"..src.." > osc"..oscnum.." freq", function(value)
+      engine."osc"..oscnum.."_to_osc"..dest.."freq", value
+    end)
   end
 
-  Ack.add_effects_params()
+  params:add_control("env1 attack", envattack_spec)
+  params:set_action("env1 attack", function(value) all_fm("envattack", value) end)
+
+  params:add_control("env1 decay", envdecay_spec)
+  params:set_action("env1 decay", function(value) all_fm("envdecay", value) end)
+
+  params:add_control("env1 sustain", envsustain_spec)
+  params:set_action("env1 sustain", function(value) all_fm("envsustain", value) end)
+
+  params:add_control("env1 release", envrelease_spec)
+  params:set_action("env1 release", function(value) all_fm("envrelease", value) end)
+
+  for oscnum=1,numoscs do
+    params:add_control("env1 > osc"..oscnum.." freq", ControlSpec.BIPOLAR)
+    params:set_action("env1 > osc"..oscnum.." freq", function(value) all_fm("env_to_osc"..oscnum.."freq", value) end)
+
+    params:add_control("env1 > osc"..oscnum.." gain", ControlSpec.UNIPOLAR)
+    params:set_action("env1 > osc"..oscnum.." gain", function(value) all_fm("env_to_osc"..oscnum.."gain", value) end)
+  end
+
+  params:add_control("lpf cutoff", ControlSpec.new(20, 10000, 'exp', 0, 10000, "Hz"))
+  params:set_action("lpf cutoff", function(value) all_poles("lpfcutoff", value) end)
+
+  params:add_control("lpf resonance", ControlSpec.UNIPOLAR)
+  params:set_action("lpf resonance", function(value) all_poles("lpfres", value) end)
+
+  params:add_control("hpf cutoff", ControlSpec.new(1, 10000, 'exp', 0, 1, "Hz"))
+  params:set_action("hpf cutoff", function(value) all_poles("hpfcutoff", value) end)
+
+  params:add_control("hpf resonance", ControlSpec.UNIPOLAR)
+  params:set_action("hpf resonance", function(value) all_poles("hpfres", value) end)
+
+  params:add_control("amp gain", ControlSpec.AMP)
+  params:set_action("amp gain", function(value) all_poles("ampgain", value) end)
+
+  params:add_control("lfo rate", ControlSpec.LOFREQ)
+  params:set_action("lfo rate", function(value) all_poles("lforate", value) end)
+
+  params:add_control("lfo > lpf cutoff", ControlSpec.BIPOLAR)
+  params:set_action("lfo > lpf cutoff", function(value) all_poles("lfo_to_lpfcutoff", value) end)
+
+  params:add_control("lfo > hpf cutoff", ControlSpec.BIPOLAR)
+  params:set_action("lfo > hpf cutoff", function(value) all_poles("lfo_to_hpfcutoff", value) end)
+
+  params:add_control("lfo > hpf resonance", ControlSpec.BIPOLAR)
+  params:set_action("lfo > hpf resonance", function(value) all_poles("lfo_to_hpfres", value) end)
+
+  params:add_control("lfo > lpf resonance", ControlSpec.BIPOLAR)
+  params:set_action("lfo > lpf resonance", function(value) all_poles("lfo_to_lpfres", value) end)
+
+  params:add_control("lfo > amp gain", ControlSpec.BIPOLAR)
+  params:set_action("lfo > amp gain", function(value) all_poles("lfo_to_ampgain", value) end)
+
+  params:add_control("env2 attack", envattack_spec)
+  params:set_action("env2 attack", function(value) all_poles("envattack", value) end)
+
+  params:add_control("env2 decay", envdecay_spec)
+  params:set_action("env2 decay", function(value) all_poles("envdecay", value) end)
+
+  params:add_control("env2 sustain", envsustain_spec)
+  params:set_action("env2 sustain", function(value) all_poles("envsustain", value) end)
+
+  params:add_control("env2 release", envrelease_spec)
+  params:set_action("env2 release", function(value) all_poles("envrelease", value) end)
+
+  params:add_control("env2 > amp gain", ControlSpec.BIPOLAR)
+  params:set_action("env2 > amp gain", function(value) all_poles("env_to_ampgain", value) end)
+
+  params:add_control("env2 > lpf cutoff", ControlSpec.BIPOLAR)
+  params:set_action("env2 > lpf cutoff", function(value) all_poles("env_to_lpfcutoff", value) end)
+
+  params:add_control("env2 > lpf resonance", ControlSpec.BIPOLAR)
+  params:set_action("env2 > lpf resonance", function(value) all_poles("env_to_lpfres", value) end)
+
+  params:add_control("env2 > hpf cutoff", ControlSpec.BIPOLAR)
+  params:set_action("env2 > hpf cutoff", function(value) all_poles("env_to_hpfcutoff", value) end)
+
+  params:add_control("env2 > hpf resonance", ControlSpec.BIPOLAR)
+  params:set_action("env2 > hpf resonance", function(value) all_poles("env_to_hpfres", value) end)
 end
 
-return Ack
+return Gong
---
+++ scripts/jah/hello_gong.lua
@@ -0,0 +1,194 @@
+-- hello_gong:
+
+-- hello gong.
+-- 6 voice polyphonic fm synth
+-- controlled by midi
+--
+-- enc2: timbre macro control
+-- enc3: time macro control
+-- key2: trig random note
+--
+-- more parameters in
+-- menu > parameters
+--
+-- midi: play notes
+--
+
+local ControlSpec = require 'controlspec'
+local Voice = require 'exp/voice'
+local Gong = require 'jah/gong'
+
+engine.name = 'Gong'
+
+local midinote_indicator_level = 0
+local midicc_indicator_level = 0
+local note_downs = {}
+
+local function screen_update_voice_indicators()
+  screen.move(0,16)
+  screen.font_size(8)
+  for voicenum=1,POLYPHONY do
+    if note_downs[voicenum] then
+      screen.level(15)
+    else
+      screen.level(2)
+    end
+    screen.text(voicenum)
+  end
+end
+
+local function screen_update_midi_indicators()
+  screen.move(0,60)
+  screen.font_size(8)
+  if midi_available then
+    screen.level(15)
+    screen.text("midi:")
+    screen.text(" ")
+    screen.level(midinote_indicator_level)
+    screen.text("note ")
+    screen.level(midicc_indicator_level)
+    screen.text("cc")
+  else
+    screen.level(3)
+    screen.text("no midi")
+  end
+end
+
+local function trig_voice(voicenum, note)
+  engine.noteOn(voicenum, note)
+end
+
+local function release_voice(voicenum)
+  engine.noteOff(voicenum)
+end
+
+local noteslots = {}
+
+local function note_on(note, velocity)
+  if not noteslots[note] then
+    local slot = voice:get()
+    local voicenum = slot.id
+    trig_voice(voicenum, note)
+    slot.on_release = function()
+      release_voice(voicenum)
+      noteslots[note] = nil
+    end
+    noteslots[note] = slot
+    note_downs[voicenum] = true
+    redraw()
+  end
+end
+
+local function note_off(note)
+  slot = noteslots[note]
+  if slot then
+    voice:release(slot)
+    note_downs[slot.id] = false
+    redraw()
+  end
+end
+
+local function default_patch()
+  params:set("osc2 > osc3 freq", 1)
+  params:set("osc2 partial no", 2)
+  params:set("osc3 gain", 1)
+  params:set("osc3 index", 5)
+  params:set("osc3 > out", 0.1)
+  params:set("env1 > osc2 gain", 1)
+  params:set("env2 > amp gain", 1)
+  params:set("delay send", -20)
+  params:set("delay time left", 0.03)
+  params:set("delay time right", 0.05)
+  params:set("delay feedback", -30)
+end
+
+function init()
+  Gong.add_params()
+
+  voice = Voice.new(POLYPHONY)
+  -- params:read("gong.pset")
+
+  screen.line_width(1.0)
+end
+
+function redraw()
+  screen.clear()
+  screen.aa(1)
+  screen.move(0, 8)
+  screen.font_size(8)
+  screen.level(15)
+  screen.text("hello gong")
+  screen_update_voice_indicators()
+  screen_update_midi_indicators()
+  screen.update()
+end
+
+enc = function(n, delta)
+  if n == 1 then
+    norns.audio.adjust_output_level(delta)
+  elseif n == 2 then
+    params:delta("timbre", delta)
+  elseif n == 3 then
+    params:delta("timemod", delta)
+  end
+end
+
+key = function(n, z)
+  if n == 2 and z == 1 then
+    note_on(60, 100)
+  elseif n == 2 and z == 0 then
+    note_off(60)
+  elseif n == 3 and z == 1 then
+    note_on(64, 100)
+  elseif n == 3 and z == 0 then
+    note_off(64)
+  end
+end
+
+cleanup = function()
+  norns.midi.event = nil
+  -- params:write("gong.pset")
+end
+
+norns.midi.add = function(id, name, dev)
+  midi_available = true
+  midinote_indicator_level = 3
+  midicc_indicator_level = 3
+  redraw()
+end
+
+norns.midi.remove = function(id)
+  midi_available = false
+  redraw()
+end
+
+norns.midi.event = function(id, data)
+  status = data[1]
+  data1 = data[2]
+  data2 = data[3]
+  if status == 144 then
+    midinote_indicator_level = math.random(15)
+    --[[
+    if data1 == 0 then
+      return -- TODO: filter OP-1 bpm link oddity, is this an op-1 or norns issue?
+    end
+    ]]
+    note_on(data1, data2)
+    redraw()
+  elseif status == 128 then
+    --[[
+    if data1 == 0 then
+      return -- TODO: filter OP-1 bpm link oddity, is this an op-1 or norns issue?
+    end
+    ]]
+    note_off(data1)
+    --[[
+  elseif status == 176 then
+    midicc_indicator_level = math.random(15)
+    cc(data1, data2)
+    redraw()
+  elseif status == 224 then
+    bend(data1, data2)
+    ]]
+  end
+end
