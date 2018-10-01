a3dbf58fadb23b0d36562c31f49d05fdd008991b
alphacactus <ryan.g.hodgman@gmail.com>
2018-09-30 23:35:15 -0400

    updated wiz

--- scripts/alphacactus/wizcraft.lua
+++ scripts/alphacactus/wizcraft.lua
@@ -1,13 +1,12 @@
 -- wizcraft
--- vector drone machine
--- 
---
--- key1
--- key2 generate vector
--- key3
--- enc1
--- enc2
--- enc3
+-- key1 shift^
+-- key2 add vector / ^add 2
+-- key3 / ^clear screen
+-- enc1 output / ^
+-- enc2 drift / ^detune
+-- enc3 / ^noise
+
+g = grid.connect()
 
 engine.name = 'PolySub'
 
@@ -20,7 +19,7 @@ function get_next_id()
     return next_id
 end
 
-Vector = {id=0, x=0, y=0, xv=0, yv=0, xa=0, ya=0, s=16}
+Vector = {id=0, x=0, y=0, xv=0, yv=0, xa=0, ya=0, s=8}
 
 function Vector:new()
     local o = {}
@@ -30,8 +29,6 @@ function Vector:new()
     o.id = get_next_id()
     o.x=math.random(128)
     o.y=math.random(64)
-    --o.xv = ((math.random(1,2)*2)-3)
-    --o.yv = ((math.random(1,2)*2)-3)
     o.xa=math.random() * ((math.random(1,2)*2)-3)
     o.ya=math.random() * ((math.random(1,2)*2)-3)
 
@@ -39,23 +36,41 @@ function Vector:new()
 end
 
 function Vector:update()
-    self.xv = util.clamp(self.xv + self.xa, -1, 1)
-    self.yv = util.clamp(self.yv + self.ya, -1, 1)
+    self.xa = (math.random() * ((math.random(1,2)*2)-3))
+    self.ya = (math.random() * ((math.random(1,2)*2)-3))
+
+    self.xv = util.clamp(self.xv + self.xa, -1*params:get("drift"), 1*params:get("drift"))
+    self.yv = util.clamp(self.yv + self.ya, -1*params:get("drift"), 1*params:get("drift"))
+--    self.xv = self.xv + self.xa
+  --  self.yv = self.yv + self.ya
 
     self.x = self.x + self.xv
     self.y = self.y + self.yv
+
+    local note = ((7-(self.y/8))*5) + (self.x/8)
+    engine.start(self.id, getHzET(note))
 end
 
-function Vector:draw()
+function Vector:draw(c)
+    screen.level(c)
+
     screen.move(self.x, self.y)
-    screen.rect(self.x, self.y, self.s, self.s)
+    for i=1,self.s do
+        screen.line(self.x+math.random(2*self.s), self.y+math.random(2*self.s))
+    end
+
+    screen.fill()
 end
 
-local function getHzET(note)
+function getHzET(note)
     return 55*2^(note/12)
 end
 
 function init()
+    mode = 0
+
+    params:add_control("drift", controlspec.new(0,2,"lin",0,0,""))
+
     params:add_control("shape", controlspec.new(0,1,"lin",0,0,""))
     params:set_action("shape", function(x) engine.shape(x) end)
 
@@ -116,16 +131,17 @@ end
 function redraw()
     screen.clear()
     screen.aa(1)
-    screen.level(15)
    
     check_collisions()
     for i=1,#vectors do
         local v = vectors[i]
-        v:update()
-        v:draw()
-        if v.x > 128 or v.x < 0 or v.y > 64 or v.y < 0 then
-            engine.stop(v.id)
-            table.remove(vectors, i)
+        if v then
+            v:draw(15)
+            v:update()
+            if v.x > 128 or v.x < 0 or v.y > 64 or v.y < 0 then
+                engine.stop(v.id)
+                table.remove(vectors, i)
+            end
         end
     end
     screen.stroke()
@@ -138,14 +154,16 @@ function check_collisions()
         local v = vectors[i]
         for j=i+1,#vectors do
             local o = vectors[j]
+            
+            if o then
+                if math.abs(v.x-o.x) < v.s and math.abs(v.y-o.y) < v.s then
+                    engine.stop(o.id)
+                    table.remove(vectors, j)
 
-            if math.abs(v.x-o.x) < v.s and math.abs(v.y-o.y) < v.s then
-                engine.stop(o.id)
-                table.remove(vectors, j)
-
-                v.s = v.s + 4
-                local note = ((7-(v.y/8))*5) + (v.x/8)
-                engine.start(v.id, getHzET(note))
+                    v.s = v.s + 4
+                    local note = ((7-(v.y/8))*5) + (v.x/8)
+                    engine.start(v.id, getHzET(note))
+                end
             end
         end
     end
@@ -156,22 +174,62 @@ function enc(n, d)
     if n == 1 then
         mix:delta("output", d)
     elseif n == 2 then
-        params:delta("cut", d)
+        if mode == 1 then
+            params:delta("detune", d)
+        else
+            for i=1,#vectors do
+                local v = vectors[i]
+                if v.x > 64 then
+                    v.x = util.clamp(v.x+d, 64, 128-v.s)
+                else
+                    v.x = util.clamp(v.x-d, 0, 63-v.s)
+                end
+
+                if v.y > 32 then 
+                    v.y = util.clamp(v.y+d, 32, 64-v.s)
+                else
+                    v.y = util.clamp(v.y-d, 0, 31-v.s)
+                end
+            end
+        end
+    elseif n == 3 then
+        if mode == 1 then
+            params:delta("noise", d)
+        else
+            for i=1,#vectors do
+                local v = vectors[i]
+                v.y = util.clamp(v.y+d, 0, 64-v.s)
+            end
+        end
     end
 end
 
 function key(n, z)
-    if n == 2 then
+    if n == 1 then
+        mode = z
+    elseif n == 2 then
         if z == 1 then
-            local v = Vector:new()
-            table.insert(vectors, v)
-            local note = ((7-(v.y/8))*5) + (v.x/8)
-            engine.start(v.id, getHzET(note))
+            if mode == 1 then
+
+            else
+                local v = Vector:new()
+                table.insert(vectors, v)
+                local note = ((7-(v.y/8))*5) + (v.x/8)
+                engine.start(v.id, getHzET(note))
+            end
+        end
+    elseif n == 3 then
+        if z == 1 then
+            if mode == 1 then
+                engine.stopAll()
+                vectors = {}
+            else
+
+            end
         end
     end
 
 end
-
 
 
 
