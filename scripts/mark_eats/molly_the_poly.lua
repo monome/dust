248cbf5f20560b0f0c0a55347b692d3825c5b053
Phil Quitslund <pq@users.noreply.github.com>
2018-10-18 09:48:13 -0700

    tweaks

--- scripts/mark_eats/molly_the_poly.lua
+++ scripts/mark_eats/molly_the_poly.lua
@@ -18,7 +18,6 @@ local SCREEN_FRAMERATE = 15
 local screen_dirty = true
 
 local midi_in_device
-local bend_ranges = {2, 3, 12, 24, 48}
 
 local SUN_BASE_RADIUS = 5
 local sun_mod_radius = 0
@@ -158,7 +157,7 @@ local function midi_event(data)
     -- Pitch bend
     elseif msg.type == "pitchbend" then
       local bend_st = (util.round(msg.val / 2)) / 8192 * 2 -1 -- Convert to -1 to 1
-      local bend_range = bend_ranges[params:get("bend_range")]
+      local bend_range = params:get("bend_range")
       set_pitch_bend(bend_st * bend_range)
       
     end
@@ -224,7 +223,7 @@ function init()
   for i = 1, 16 do table.insert(channels, i) end
   params:add{type = "option", id = "midi_channel", name = "MIDI Channel", options = channels}
 
-  params:add{type = "option", id = "bend_range", name = "Bend Range", options = bend_ranges}
+  params:add{type = "number", id = "bend_range", name = "Pitch Bend Range", min = 1, max = 48, default = 2}
   
   params:add_separator()
   
