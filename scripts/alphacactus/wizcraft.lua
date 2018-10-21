0a432cf9031625d6bc7e188bb56044a794c4984b
alphacactus <ryan.g.hodgman@gmail.com>
2018-10-20 21:53:40 -0400

    updates to wizcraft.lua

--- scripts/alphacactus/wizcraft.lua
+++ scripts/alphacactus/wizcraft.lua
@@ -1,26 +1,32 @@
 -- wizcraft
 -- key1 shift^
 -- key2 add vector / ^
--- key3 / ^clear screen
+-- key3 add force / ^clear screen
 -- enc1 drift / ^output
--- enc2 push/pull / ^detune
--- enc3 up/down / ^noise
+-- enc2 push/pull / ^select scale
+-- enc3 up/down / ^detune
 
-engine.name = "PolySub"
+local MusicUtil = require "mark_eats/musicutil"
+local scale_notes = {}
+
+local MollyThePoly = require "mark_eats/mollythepoly"
+
+engine.name = "MollyThePoly"
 
 vectors = {}
-NUM_VECTORS = 16
+local NUM_VECTORS = 8
 
-forces = {}
+local forces = {}
+local NUM_FORCES = 8
 
-next_id = 0
-function get_next_id()
+local next_id = 0
+local function get_next_id()
     next_id = (next_id % NUM_VECTORS) + 1
     return next_id
 end
 
 -- return plus or minus one at random
-function flip()
+local function flip()
     return ((math.random(1,2)*2)-3)
 end
 
@@ -33,6 +39,8 @@ function Vector:new()
 
     o.x=math.random(128)
     o.y=math.random(64)
+    o.xv = 0
+    o.yv = 0
     o.xa=math.random() * flip()
     o.ya=math.random() * flip()
 
@@ -45,21 +53,107 @@ function Vector:update()
 
     self.xv = util.clamp(self.xv + self.xa, -1*params:get("drift"), 1*params:get("drift"))
     self.yv = util.clamp(self.yv + self.ya, -1*params:get("drift"), 1*params:get("drift"))
+    self.xv = util.round(self.xv, 1)
+    self.yv = util.round(self.yv, 1)
 
     self.x = self.x + self.xv
     self.y = self.y + self.yv
+
+    local sx = self.x + self.x
+    local sy = self.y + self.y
+    for i=1,#vectors do -- check for vector collisions
+        local v = vectors[i]
+        if v then
+            local vx = v.x + v.s
+            local vy = v.y + v.s
+            if self.id ~= v.id then
+                if math.abs(sx-vx) < self.s and math.abs(sy-vy) < self.s then
+                    engine.noteOff(v.id)
+                    table.remove(vectors, i)
+
+                    local n = Vector:new()
+                    n.id = get_next_id()
+                    table.insert(vectors, n)
+                    n:play_note()
+
+                    self.s = self.s + v.s
+                    self:play_note()
+                end
+            end
+        end
+    end
+
+end
+
+function Vector:update_f() 
+    self.xa = math.random() * flip() 
+    self.ya = math.random() * flip()
+
+    self.xv = util.clamp(self.xv + self.xa, -2, 2)
+    self.yv = util.clamp(self.yv + self.ya, -2, 2)
+
+    self.xv = util.round(self.xv, 1)
+    self.yv = util.round(self.yv, 1)
+
+    self.x = util.clamp(self.x + self.xv, 2, 126)
+    self.y = util.clamp(self.y + self.yv, 2, 62)
+
+    local sx = self.x + self.s
+    local sy = self.y + self.s
+    for i=1,#vectors do
+        local v = vectors[i]
+        if v then
+            local vx = v.x + v.s
+            local vy = v.y + v.s
+
+            local dx = math.abs(sx-vx)        
+            local dy = math.abs(sy-vy)
+            if dx <= 2 and dy <= 2 then
+                engine.noteOff(v.id)
+                table.remove(vectors, i)
+
+                MollyThePoly.randomize_params("lead")
+                MollyThePoly.randomize_params("pad")
+                MollyThePoly.randomize_params("percussion")
+
+                local n = Vector:new()
+                n.id = get_next_id()
+                table.insert(vectors, n)
+                n:play_note()
+            elseif dx <= 16 and dy <= 16 then
+                if sx > vx then
+                    v.x = v.x + 4
+                else 
+                    v.x = v.x - 4
+                end
+
+                if sy > vy then
+                    v.y = v.y + 4
+                else
+                    v.y = v.y - 4
+                end
+                vectors[i] = v
+                vectors[i]:play_note()
+            end
+        end
+    end
 end
 
 function Vector:play_note()
-    local note = ((7-(self.y/8))*5) + (self.x/8)
-    engine.start(self.id, 55*2^(note/12))
+    local note = util.round(util.linlin(0, 128, 1, #scale_notes, self.x), 1)
+    local octave = util.round(util.linlin(0, 64, 1, 7, self.y), 1)
+    local note_num = scale_notes[note] + (octave*12)
+    local freq = MusicUtil.note_num_to_freq(note_num)
+    --[[local note = ((7-(self.y/8))*5) + (self.x/8)
+    freq =  55*2^(note/12)]]--
+    engine.noteOn(self.id, freq, 1)
 end
 
 function Vector:draw(c)
     screen.level(c)
 
-    local x = self.x+self.s
-    local y = self.y+self.s
+    local x = self.x + self.s
+    local y = self.y + self.s
     screen.move(x, y)
     for i=1,self.s do
         screen.line(x+(math.random(self.s)*flip()), y+(math.random(self.s)*flip()))
@@ -68,58 +162,26 @@ function Vector:draw(c)
     screen.fill()
 end
 
+function Vector:draw_f(c)
+    screen.level(c)
+
+    local x = self.x + self.s
+    local y = self.y + self.s
+    screen.move(x, y)
+    for i=1,self.s do
+        screen.arc(x, y, self.s, math.pi*math.random(), 2*math.pi*math.random()) 
+    end
+end
 
 function init()
     tmp = 1
     shift = 0
+    scale_notes = MusicUtil.generate_scale(0, scale_type, 1)
 
-    params:add_control("drift", controlspec.new(0,2,"lin",0,0,""))
+    params:add_number("scale_type", "scale_type", 1, #MusicUtil.SCALES, 1)
 
-    params:add_control("shape", controlspec.new(0,1,"lin",0,0,""))
-    params:set_action("shape", function(x) engine.shape(x) end)
-
-    params:add_control("timbre", controlspec.new(0,1,"lin",0,0.5,""))
-    params:set_action("timbre", function(x) engine.timbre(x) end)
-
-    params:add_control("noise", controlspec.new(0,1,"lin",0,0,""))
-    params:set_action("noise", function(x) engine.noise(x) end)
-
-    params:add_control("cut", controlspec.new(0,32,"lin",0,8,""))
-    params:set_action("cut", function(x) engine.cut(x) end)
-
-    params:add_control("fgain", controlspec.new(0,6,"lin",0,0,""))
-    params:set_action("fgain", function(x) engine.fgain(x) end)
-
-    params:add_control("cutEnvAmt", controlspec.new(0,1,"lin",0,0,""))
-    params:set_action("cutEnvAmt", function(x) engine.cutEnvAmt(x) end)
-
-    params:add_control("detune", controlspec.new(0,1,"lin",0,0,""))
-    params:set_action("detune", function(x) engine.detune(x) end)
-
-    params:add_control("ampAtk", controlspec.new(0.01,10,"lin",0,0.05,""))
-    params:set_action("ampAtk", function(x) engine.ampAtk(x) end)
-
-    params:add_control("ampDec", controlspec.new(0,2,"lin",0,0.1,""))
-    params:set_action("ampDec", function(x) engine.ampDec(x) end)
-
-    params:add_control("ampSus", controlspec.new(0,1,"lin",0,1,""))
-    params:set_action("ampSus", function(x) engine.ampSus(x) end)
-
-    params:add_control("ampRel", controlspec.new(0.01,10,"lin",0,1,""))
-    params:set_action("ampRel", function(x) engine.ampRel(x) end)
-
-    params:add_control("cutAtk", controlspec.new(0.01,10,"lin",0,0.05,""))
-    params:set_action("cutAtk", function(x) engine.cutAtk(x) end)
-
-    params:add_control("cutDec", controlspec.new(0,2,"lin",0,0.1,""))
-    params:set_action("cutDec", function(x) engine.cutDec(x) end)
-
-    params:add_control("cutSus", controlspec.new(0,1,"lin",0,1,""))
-    params:set_action("cutSus", function(x) engine.cutSus(x) end)
-
-    params:add_control("cutRel", controlspec.new(0.01,10,"lin",0,1,""))
-    params:set_action("cutRel", function(x) engine.cutRel(x) end)
-
+    params:add_control("drift", "drift", controlspec.new(0,2,"lin",0,0,""))
+    MollyThePoly.add_params()
 
     clk = metro.alloc()
     clk.time = 1/15
@@ -138,48 +200,33 @@ function redraw()
     screen.clear()
     screen.aa(1)
    
-    check_collisions()
     for i=1,#vectors do
         local v = vectors[i]
         if v then
             v:draw(15)
             v:update()
             if v.x > 128 or v.x < 0 or v.y > 64 or v.y < 0 then
-                engine.stop(v.id)
+                engine.noteOff(v.id)
                 table.remove(vectors, i)
+
+                local n = Vector:new()
+                n.id = get_next_id()
+                table.insert(vectors, n)
+                n:play_note()
             end
         end
     end
     screen.close()
     screen.stroke()
 
-    screen.update()
-end
-
-
-function check_collisions()
-    for i=1,#vectors do
-        local v = vectors[i]
-        if v then
-            local vx = v.x + v.s
-            local vy = v.y + v.s
-            for j=i+1,#vectors do
-                local o = vectors[j]
-                if o then
-                    local ox = o.x + o.s
-                    local oy = o.y + o.s
-                    
-                    if math.abs(vx-ox) < v.s and math.abs(vy-oy) < v.s then
-                        engine.stop(o.id)
-                        table.remove(vectors, j)
-
-                        v.s = v.s + 4
-                        v:play_note()
-                    end
-                end
-            end
-        end
+    for i=1,#forces do
+        local f = forces[i]
+        f:draw_f(15)
+        f:update_f()
     end
+    screen.close()
+    screen.stroke()
+    screen.update()
 end
 
 
@@ -188,9 +235,11 @@ function enc(n, d)
         if n == 1 then
             mix:delta("output", d)
         elseif n == 2 then
-            params:delta("detune", d)
+            params:delta("scale_type", d)
+            scale_notes = MusicUtil.generate_scale(0, params:get("scale_type"), 1)
+            tab.print(scale_notes)
         elseif n == 3 then
-            params:delta("noise", d)
+            params:delta("detune", d)
         end
     else
         if n == 1 then
@@ -198,25 +247,29 @@ function enc(n, d)
         elseif n == 2 then
             for i=1,#vectors do
                 local v = vectors[i]
-                if v.x > 64 then
-                    v.x = util.clamp(v.x+d, 64, 128-v.s)
-                else
-                    v.x = util.clamp(v.x-d, 0, 63-v.s)
-                end
+                if v then
+                    if v.x > 64 then
+                        v.x = util.clamp(v.x+d, 64, 128-v.s)
+                    else
+                        v.x = util.clamp(v.x-d, 0, 63-v.s)
+                    end
 
-                if v.y > 32 then 
-                    v.y = util.clamp(v.y+d, 32, 64-v.s)
-                else
-                    v.y = util.clamp(v.y-d, 0, 31-v.s)
-                end
+                    if v.y > 32 then 
+                        v.y = util.clamp(v.y+d, 32, 64-v.s)
+                    else
+                        v.y = util.clamp(v.y-d, 0, 31-v.s)
+                    end
 
-                v:play_note()
+                    v:play_note()
+                end
             end
         elseif n == 3 then
             for i=1,#vectors do
                 local v = vectors[i]
-                v.y = util.clamp(v.y+d, 0, 64-v.s)
-                v:play_note()
+                if v then
+                    v.y = util.clamp(v.y+d, 0, 64-v.s)
+                    v:play_note()
+                end
             end
         end
     end
@@ -233,8 +286,9 @@ function key(n, z)
             local f = Vector:new()
             table.insert(forces, f)
         elseif n == 3 then -- clear all vectors
-            engine.stopAll()
+            engine.noteOffAll()
             vectors = {}
+            forces = {}
         end
     else
         if n == 2 then -- add vector
@@ -245,7 +299,10 @@ function key(n, z)
                 v:play_note()
             end
         elseif n == 3 then 
-            -- empty for now
+            if z == 1 then
+                local f = Vector:new()
+                table.insert(forces, f)
+            end
         end
     end
 end
