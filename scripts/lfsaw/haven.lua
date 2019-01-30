5014fcb1e932ea2182c1d050fe3ad37cdc1ca945
Artem Popov <artfwo@gmail.com>
2019-01-31 05:43:41 +0700

    Update haven ui

--- scripts/lfsaw/haven.lua
+++ scripts/lfsaw/haven.lua
@@ -1,8 +1,9 @@
 -- haven.
+--
 -- a safe space?
 --
 
-engine.name = 'Haven'
+engine.name = "Haven"
 
 local sel = 1
 local shift = false
@@ -11,51 +12,60 @@ function init()
   params:add{
     type="control",
     id="freq1",
-    controlspec=controlspec.new(10, 200, "exp", 0, 20, "hz"),
+    controlspec=controlspec.new(1, 800, "exp", 0, 20, "Hz"),
     action=engine.freq1,
   }
 
   params:add{
     type="control",
     id="freq2",
-    controlspec=controlspec.new(1000, 12000, "exp", 0, 4000, "hz"),
+    controlspec=controlspec.new(400, 12000, "exp", 0, 4000, "Hz"),
     action=engine.freq2,
   }
 
   params:add{
     type="control",
     id="amp1",
-    controlspec=controlspec.new(-90, 0, "linear", 0, 0, ""),
+    controlspec=controlspec.new(-90, 0, "db", 0, -90, "dB"),
     action=engine.amp1,
   }
 
   params:add{
     type="control",
     id="amp2",
-    controlspec=controlspec.new(-90, 0, "linear", 0, 0, ""),
+    controlspec=controlspec.new(-90, 0, "db", 0, -90, "dB"),
     action=engine.amp2,
   }
 
   params:add{
     type="control",
     id="in_amp",
-    controlspec=controlspec.new(-90, 0, "linear", 0, 0, ""),
+    controlspec=controlspec.new(-90, 0, "db", 0, -90, "dB"),
     action=engine.inAmp,
   }
 
   params:add{
     type="control",
     id="fdbck",
-    controlspec=controlspec.new(0, 1, "linear", 0, 0.03, ""),
-    action=engine.fdbck,
+    controlspec=controlspec.new(-1, 1, "linear", 0, 0.03, ""),
+    action=function(value)
+      if (value < 0) then
+        engine.fdbckSign(-1)
+      else
+        engine.fdbckSign(1)
+      end
+      engine.fdbck(math.abs(value))
+    end,
   }
-  
+
   params:add{
     type="control",
     id="rev_level",
-    controlspec=controlspec.new(-math.huge,18,'db',0,0,"dB"),
+    controlspec=controlspec.new(-math.huge, 18, "db", 0, 0, "dB"),
     action=function(value) mix:set("rev_level", value) end,
   }
+
+  params:bang()
 end
 
 function redraw()
@@ -64,64 +74,25 @@ function redraw()
   screen.font_size(8)
   screen.level(15)
 
-  screen.level(sel == 1 and 15 or 4)
-  screen.move(30, 8)
-  screen.text(params:string("freq1"))
+  screen.level(sel == 1 and 15 or 2)
+  screen.move(8, 16)
+  screen.text("lo: "..params:string("freq1").."  *  "..params:string("amp1"))
 
-  screen.level(3)
-  screen.move(0, 8)
-  screen.text("lo freq")
-
-  screen.level(sel == 1 and 15 or 4)
-  screen.move(94, 8)
-  screen.text(params:string("amp1"))
-
-  screen.level(3)
-  screen.move(74, 8)
-  screen.text("amp")
-
-  screen.level(sel == 2 and 15 or 4)
-  screen.move(30, 24)
-  screen.text(params:string("freq2"))
-
-  screen.level(3)
-  screen.move(0, 24)
-  screen.text("hi freq")
-
-  screen.level(sel == 2 and 15 or 4)
-  screen.move(94, 24)
-  screen.text(params:string("amp2"))
-
-  screen.level(3)
-  screen.move(74, 24)
-  screen.text("amp")
-
-  screen.level(sel == 3 and 15 or 4)
-  screen.move(30, 40)
-  screen.text(params:string("fdbck"))
-
-  screen.level(3)
-  screen.move(0, 40)
-  screen.text("fdbck")
-
-  screen.level(sel == 3 and 15 or 4)
-  screen.move(64, 56)
-  screen.text(params:string("in amp"))
-
-  screen.level(3)
-  screen.move(74, 40)
-  screen.text("in")
-
-  screen.level(sel == 4 and 15 or 4)
-  screen.move(30, 56)
-  screen.text(params:string("rev_level"))
+  screen.level(sel == 2 and 15 or 2)
+  screen.move(8, 24)
+  screen.text("hi: "..params:string("freq2").."  *  "..params:string("amp2"))
   
-  screen.level(3)
-  screen.move(0, 56)
-  screen.text("reverb")
+  screen.level(sel == 3 and 15 or 2)
+  screen.move(8, 32)
+  screen.text("fdbck: "..params:string("fdbck"))
 
-  -- screen.move(128, 8)
-  -- screen.text_right("haven")
+  screen.level(sel == 4 and 15 or 2)
+  screen.move(8, 40)
+  screen.text("in: "..params:string("in_amp"))
+
+  screen.level(sel == 5 and 15 or 2)
+  screen.move(8, 48)
+  screen.text("reverb: "..params:string("rev_level"))
 
   screen.update()
 end
@@ -130,7 +101,11 @@ end
 function key(n, z)
   if n == 2 and z == 1 then
     sel = sel + 1
-    if sel > 4 then sel = 1 end
+
+    if sel > 5 then
+      sel = 1
+    end
+
     redraw()
   end
   
@@ -165,19 +140,20 @@ function enc(n, delta)
       params:delta("amp2", delta)
     end
   elseif sel == 3 then
-    if n == 2 then
+    if n == 2 or n == 3 then
       if shift then
         params:set("fdbck", -params:get("fdbck"))
       else
         params:delta("fdbck", delta)
       end
     end
-    if n == 3 then
-      if shift then delta = delta / 10 end
+  elseif sel == 4 then
+    if n == 2 or n == 3 then
+      if shift then delta = delta / 100 end
       params:delta("in_amp", delta)
     end
-  elseif sel == 4 then
-    if n == 2 then
+  elseif sel == 5 then
+    if n == 2 or n == 3 then
       if shift then delta = delta / 100 end
       params:delta("rev_level", delta)
     end
